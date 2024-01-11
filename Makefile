d0f6666b292ec845d1f726b83dc292c520c76c12
Paula <polagorge@gmail.com>
2024-01-11 23:14:13 +0100

    now we can send stdin

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 bserver:
 	g++ server.cpp ./serverHelpers/socket.c ./serverHelpers/readFromclient.c -o server && ./server 4000
 bclient:
-	g++ client.cpp ./clientHelpers/clientCommunication.c -o client && ./client
+	g++ client.cpp ./clientHelpers/clientCommunication.cpp -o client && ./client
 tbclient:
 	strace -f g++ client.cpp ./clientHelpers/clientCommunication.c -o client && ./client
 tbserver:
--- clientHelpers/clientCommunication.cpp
+++ clientHelpers/clientCommunication.cpp
@@ -5,18 +5,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>  // Include for close function
-
+#include <iostream>
+#include <string>
+using namespace std;
 void clientCommunication(int sock){
-    const char *message = "Hello from client";
+    char message[1024] ;
     char buffer[1024] = {0};
     
     while(true) {
+        fgets(message, 1024, stdin);
         send(sock, message, strlen(message), 0);  // Send message to server
         printf("Message sent\n");
         int read_val = read(sock, buffer, 1024);  // Read response from server
         if(read_val <= 0) break;  // Exit loop if read error or server disconnects
 
         printf("Message from server: %s\n", buffer);
-        sleep(1); // Delay to prevent flooding the server
     }
 }
