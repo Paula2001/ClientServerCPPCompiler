74179edd554c221f964244699b6db8415e96d2d1
Paula <polagorge@gmail.com>
2024-01-12 01:58:18 +0100

    finished till compiling the file

--- clientHelpers/clientCommunication.cpp
+++ clientHelpers/clientCommunication.cpp
@@ -7,18 +7,43 @@
 #include <unistd.h>  // Include for close function
 #include <iostream>
 #include <string>
+
 using namespace std;
+
+void sendMessage(int sock){
+    char message[1024] ;
+    char buffer[1024];
+    FILE *file;
+    memset(message, 0, sizeof(message));  // Clear the buffer
+    memset(buffer, 0, sizeof(buffer));  // Clear the buffer
+    if(fgets(message, 1024, stdin) != NULL){
+        size_t length = strlen(message);
+        message[length - 1] = '\0'; // INF: terminate the bytes
+        
+        file = fopen(message, "rb"); // TODO : add this to a new function
+        if (file == NULL) {
+            perror("File not found");
+            exit(1);
+        }
+
+        while (!feof(file)) {
+            int bytesRead = fread(buffer, 1, 1024, file);
+            // buffer[strlen(buffer) - 1] = '\0';
+            if (bytesRead > 0) {
+                send(sock, buffer, bytesRead, 0);
+            }
+        }
+    };
+}
+
 void clientCommunication(int sock){
     char message[1024] ;
     char buffer[1024] = {0};
     
     while(true) {
-        memset(message, 0, sizeof(message));  // Clear the buffer
         memset(buffer, 0, sizeof(buffer));  // Clear the buffer
-
-        if(fgets(message, 1024, stdin) != NULL){
-            send(sock, message, strlen(message), 0);  // Send message to server
-        };
+        sendMessage(sock);
+        
         printf("Message sent\n");
         int read_val = read(sock, buffer, 1024);  // Read response from server
         if(read_val <= 0) break;  // Exit loop if read error or server disconnects
--- serverHelpers/readFromclient.c
+++ serverHelpers/readFromclient.c
@@ -4,6 +4,30 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+
+void createANewFileAndAppendToIt(char* buffer){
+    FILE* f = fopen("./testing/aTest2.cpp", "w");
+    // Write some text to the file
+    fprintf(f, buffer);// Close the file
+    fclose(f);
+}
+
+void compile(){
+    system("g++ ./testing/aTest2.cpp -o test3");
+}
+
+void readAndSendCompiledFile(int sock){
+    char* buffer[1024];
+    FILE* file = fopen("./test", "rb");
+    while (!feof(file)) {
+        int bytesRead = fread(buffer, 1, 1024, file);
+        // buffer[strlen(buffer) - 1] = '\0';
+        if (bytesRead > 0) {
+            send(sock, buffer, bytesRead, 0);
+        }
+    }
+}
+
 void readFromClient(int new_socket){
     char buffer[1024] = {0};
     const char *message = "Hello from server";
@@ -13,14 +37,15 @@ void readFromClient(int new_socket){
         exit(EXIT_FAILURE);
     }
     if(x == 0){
-        while(true){
-                memset(buffer, 0, sizeof(buffer));  // Clear the buffer
-                int read_val = read(new_socket, buffer, 1024);  // Read message from client
-                if(read_val <= 0) break;  // Exit loop if read error or client disconnects
-
-                printf("Message from client: %s\n", buffer);
-                send(new_socket, message, strlen(message), 0);  // Send message to client
         
+        while(1){
+            memset(buffer, 0, sizeof(buffer));  // Clear the buffer
+            int read_val = recv(new_socket, buffer, 1024, 0);  // Read message from client
+            if(read_val <= 0) break;  // Exit loop if read error or client disconnects
+            createANewFileAndAppendToIt(buffer);
+            compile();
+            printf("%s", buffer);
+            send(new_socket, message, strlen(message), 0);  // Send message to client
         }
     }
 }
